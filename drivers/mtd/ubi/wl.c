b003f9a5ab68105332797b3c86ad9386cdb72dd9
Tatyana Brokhman <tlinder@codeaurora.org>
2014-04-11 07:20:58 +0300

    mtd: ubi: Fill read disturb statistics
    Fill in eraseblock statistics needed for read disturb decision making:
    1. read counter: incremented at each read operation
    		 reset at each erase operation
        Saved only as part of the meta data in RAM
        On attach: if fastmap data is missing a default value is assigned
    2. last erase time stamp: updated at each erase operation.
       Saved as part of PEB OOB info on flash
       On attach: if fastmap data is missing retrieved from PEB EC Header
       on NAND.
       If fastmap data is corrupted a default value is assigned.
    
    The above parameters are saved as part of the fastmap data.
    
    Change-Id: I298ecd8f5d0849cb044fafa2854f234b553c8b16
    Signed-off-by: Tatyana Brokhman <tlinder@codeaurora.org>
    Signed-off-by: Dolev Raviv <draviv@codeaurora.org>

--- drivers/mtd/ubi/wl.c
+++ drivers/mtd/ubi/wl.c
@@ -105,6 +105,7 @@
 #include <linux/crc32.h>
 #include <linux/freezer.h>
 #include <linux/kthread.h>
+#include <linux/time.h>
 #include "ubi.h"
 
 /* Number of physical eraseblocks reserved for wear-leveling purposes */
@@ -740,6 +741,7 @@ static int sync_erase(struct ubi_device *ubi, struct ubi_wl_entry *e,
 	int err;
 	struct ubi_ec_hdr *ec_hdr;
 	unsigned long long ec = e->ec;
+	struct timeval tv;
 
 	dbg_wl("erase PEB %d, old EC %llu", e->pnum, ec);
 
@@ -771,11 +773,16 @@ static int sync_erase(struct ubi_device *ubi, struct ubi_wl_entry *e,
 
 	ec_hdr->ec = cpu_to_be64(ec);
 
+	do_gettimeofday(&tv);
+	/* The last erase time resolution is in days */
+	ec_hdr->last_erase_time = cpu_to_be64(tv.tv_sec / NUM_SEC_IN_DAY);
 	err = ubi_io_write_ec_hdr(ubi, e->pnum, ec_hdr);
 	if (err)
 		goto out_free;
 
 	e->ec = ec;
+	e->last_erase_time = tv.tv_sec / NUM_SEC_IN_DAY;
+	e->rc = 0;
 	spin_lock(&ubi->wl_lock);
 	if (e->ec > ubi->max_ec)
 		ubi->max_ec = e->ec;
@@ -977,6 +984,8 @@ int ubi_wl_put_fm_peb(struct ubi_device *ubi, struct ubi_wl_entry *fm_e,
 		ubi->lookuptbl[pnum] = e;
 	} else {
 		e->ec = fm_e->ec;
+		e->rc = fm_e->rc;
+		e->last_erase_time = fm_e->last_erase_time;
 		kfree(fm_e);
 	}
 
@@ -1906,6 +1915,19 @@ int ubi_wl_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 
 		e->pnum = aeb->pnum;
 		e->ec = aeb->ec;
+		e->last_erase_time = aeb->last_erase_time;
+		e->rc = aeb->rc;
+		if (!ubi->fm) {
+			if (e->rc < UBI_MAX_READCOUNTER) {
+				e->rc++;
+			} else {
+				ubi_err("read counter overflow at PEB %d, RC %d",
+					e->pnum, e->rc);
+				kmem_cache_free(ubi_wl_entry_slab, e);
+				goto out_free;
+			}
+
+		}
 		ubi_assert(!ubi_is_fm_block(ubi, e->pnum));
 		ubi->lookuptbl[e->pnum] = e;
 		if (schedule_erase(ubi, e, aeb->vol_id, aeb->lnum, 0)) {
@@ -1926,6 +1948,19 @@ int ubi_wl_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 
 		e->pnum = aeb->pnum;
 		e->ec = aeb->ec;
+		e->rc = aeb->rc;
+		if (!ubi->fm) {
+			if (e->rc < UBI_MAX_READCOUNTER) {
+				e->rc++;
+			} else {
+				ubi_err("rc overflow at PEB %d, RC %d",
+						e->pnum, e->rc);
+				kmem_cache_free(ubi_wl_entry_slab, e);
+				goto out_free;
+			}
+
+		}
+		e->last_erase_time = aeb->last_erase_time;
 		ubi_assert(e->ec >= 0);
 		ubi_assert(!ubi_is_fm_block(ubi, e->pnum));
 
@@ -1947,6 +1982,18 @@ int ubi_wl_init(struct ubi_device *ubi, struct ubi_attach_info *ai)
 
 			e->pnum = aeb->pnum;
 			e->ec = aeb->ec;
+			e->rc = aeb->rc;
+			if (!ubi->fm) {
+				if (e->rc < UBI_MAX_READCOUNTER) {
+					e->rc++;
+				} else {
+					ubi_err("rc overflow at PEB %d, RC %d",
+							e->pnum, e->rc);
+					kmem_cache_free(ubi_wl_entry_slab, e);
+					goto out_free;
+				}
+			}
+			e->last_erase_time = aeb->last_erase_time;
 			ubi->lookuptbl[e->pnum] = e;
 
 			if (!aeb->scrub) {
