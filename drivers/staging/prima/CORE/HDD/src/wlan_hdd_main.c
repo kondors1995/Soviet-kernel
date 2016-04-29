22775925af23385a635a35a9308a1ab00c440ffd
James Christopher Adduono <jc@adduono.com>
2016-04-29 04:15:22 -0400

    Prevent panic reboot on disconnect of wlan1

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -8334,7 +8334,7 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
 
    // Unregister the Net Device Notifier
    //unregister_netdevice_notifier(&hdd_netdev_notifier);
-
+   
    //hdd_stop_all_adapters( pHddCtx );
 
 #ifdef WLAN_BTAMP_FEATURE
@@ -9551,6 +9551,7 @@ int hdd_wlan_startup(struct device *dev )
    }
 
    // register net device notifier for device change notification
+/*
    ret = register_netdevice_notifier(&hdd_netdev_notifier);
 
    if(ret < 0)
@@ -9558,12 +9559,13 @@ int hdd_wlan_startup(struct device *dev )
       hddLog(VOS_TRACE_LEVEL_ERROR,"%s: register_netdevice_notifier failed",__func__);
       goto err_free_power_on_lock;
    }
+*/
 
    //Initialize the nlink service
    if(nl_srv_init() != 0)
    {
       hddLog(VOS_TRACE_LEVEL_FATAL,"%s: nl_srv_init failed", __func__);
-      goto err_reg_netdev;
+      goto err_nl_srv;
    }
 
 #ifdef WLAN_KD_READY_NOTIFIER
@@ -9707,8 +9709,8 @@ err_nl_srv:
 #else
    nl_srv_exit();
 #endif /* WLAN_KD_READY_NOTIFIER */
-err_reg_netdev:
-   unregister_netdevice_notifier(&hdd_netdev_notifier);
+//err_reg_netdev:
+//   unregister_netdevice_notifier(&hdd_netdev_notifier);
 
 err_free_power_on_lock:
    free_riva_power_on_lock("wlan");
